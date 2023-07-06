3b1b327f1962d2fd78827920a03a6e0527e11c39
Clément ABABOU <clement.ababou@outlook.com>
2023-07-07 01:34:03 +0200

    Bumped version number to 6.7.0 and 81

--- src/main/java/fr/klemms/slotmachine/SlotPlugin.java
+++ src/main/java/fr/klemms/slotmachine/SlotPlugin.java
@@ -41,7 +41,7 @@ public class SlotPlugin extends JavaPlugin {
 	public static Economy econ = null;
 	public static VotingPluginHooks votingPlugin = null;
 	public static final String BRANCH = "release";
-	public static final int VERSION = 80;
+	public static final int VERSION = 81;
 	public static Metrics metrics;
 	public static boolean supportEnding = false;
 	public static String supportMessage = "";
