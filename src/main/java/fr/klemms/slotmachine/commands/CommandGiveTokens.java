79c5a80f85567fb3f0265f1b57854cbde3554b7a
Clément ABABOU <clement.ababou@outlook.com>
2024-07-28 14:28:04 +0200

    Fixed exception when giving tokens

--- src/main/java/fr/klemms/slotmachine/commands/CommandGiveTokens.java
+++ src/main/java/fr/klemms/slotmachine/commands/CommandGiveTokens.java
@@ -3,8 +3,6 @@ package fr.klemms.slotmachine.commands;
 import fr.klemms.slotmachine.ChatContent;
 import fr.klemms.slotmachine.Config;
 import fr.klemms.slotmachine.SlotPlugin;
-import fr.klemms.slotmachine.tokens.Token;
-import fr.klemms.slotmachine.tokens.TokenSelectionListener;
 import fr.klemms.slotmachine.tokens.TokensInventory;
 import fr.klemms.slotmachine.utils.ItemStackUtil;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -21,48 +19,40 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class CommandGiveTokens implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length >= 2) {
-			List<Player> players = new ArrayList<Player>();
-			if(args[0].equalsIgnoreCase("@a")) {
-				players.addAll(Bukkit.getOnlinePlayers());
-			} else if(args[0].equalsIgnoreCase("@r")) {
-				players.add((new ArrayList<Player>(Bukkit.getOnlinePlayers())).get(ThreadLocalRandom.current().nextInt(0, Bukkit.getOnlinePlayers().size())));
-			} else if(Bukkit.getPlayer(args[0]) != null) {
-				players.add(Bukkit.getPlayer(args[0]));
-			} else {
-				sender.sendMessage(ChatContent.RED + SlotPlugin.CHAT_PREFIX + "This player does not exist or is not online. Please use <player name> or @a or @r");
-				return true;
-			}
-			if(players.size() > 0) {
-				if(NumberUtils.isCreatable(args[1])) {
-					for(Player player : players) {
-						ItemStack is = Config.tokens.get("default");
-						final int amount = Integer.valueOf(args[1]);
-						if (args.length == 3 && Config.tokens.containsKey(args[2])) {
-							is = Config.tokens.get(args[2]);
-							player.getInventory().addItem(ItemStackUtil.changeItemStackAmount(new ItemStack(is), amount));
-							player.updateInventory();
-							sender.sendMessage(ChatContent.GREEN + SlotPlugin.CHAT_PREFIX + "You have successfully given " + amount + " token '" + args[2] + "' to " + player.getName());
-						} else {
-							TokensInventory.showManagementScreen(player, 0, "Pick a Token to give", "Left click to pick this Token", new TokenSelectionListener() {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length >= 2) {
+            List<Player> players = new ArrayList<Player>();
+            if (args[0].equalsIgnoreCase("@a")) {
+                players.addAll(Bukkit.getOnlinePlayers());
+            } else if (args[0].equalsIgnoreCase("@r")) {
+                players.add((new ArrayList<Player>(Bukkit.getOnlinePlayers())).get(ThreadLocalRandom.current().nextInt(0, Bukkit.getOnlinePlayers().size())));
+            } else if (Bukkit.getPlayer(args[0]) != null) {
+                players.add(Bukkit.getPlayer(args[0]));
+            } else {
+                sender.sendMessage(ChatContent.RED + SlotPlugin.CHAT_PREFIX + "This player does not exist or is not online. Please use <player name> or @a or @r");
+                return true;
+            }
+            if (!players.isEmpty()) {
+                if (NumberUtils.isCreatable(args[1])) {
+                    for (Player player : players) {
+                        final int amount = Integer.parseInt(args[1]);
 
-								@Override
-								public void callback(Player player, Token token) {
-									player.closeInventory();
-									player.getInventory().addItem(ItemStackUtil.changeItemStackAmount(new ItemStack(token.itemStack), amount));
-									player.updateInventory();
-									sender.sendMessage(ChatContent.GREEN + SlotPlugin.CHAT_PREFIX + "You have successfully given " + amount + " token '" + args[2] + "' to " + player.getName());
-								}
-
-							}, false);
-						}
-					}
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+                        if (args.length == 3 && Config.tokens.containsKey(args[2])) {
+                            player.getInventory().addItem(ItemStackUtil.changeItemStackAmount(new ItemStack(Config.tokens.get(args[2])), amount));
+                            sender.sendMessage(ChatContent.GREEN + SlotPlugin.CHAT_PREFIX + "You have successfully given " + amount + " token '" + args[2] + "' to " + player.getName());
+                        } else {
+                            TokensInventory.showManagementScreen(player, 0, "Pick a Token to give", "Left click to pick this Token", (player1, token) -> {
+                                player1.closeInventory();
+                                player1.getInventory().addItem(ItemStackUtil.changeItemStackAmount(new ItemStack(token.itemStack), amount));
+                                sender.sendMessage(ChatContent.GREEN + SlotPlugin.CHAT_PREFIX + "You have successfully given " + amount + " token '" + token.identifier + "' to " + player1.getName());
+                            }, false);
+                        }
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
